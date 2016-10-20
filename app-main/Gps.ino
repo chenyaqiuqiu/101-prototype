c305bf20877baa3e81a5727cde3c03c239b53516
chenyang <chenyang.ma@nanchao.org>
2016-10-20 16:53:34 +0800

    add gps api

--- app-main/Gps.ino
+++ app-main/Gps.ino
@@ -6,8 +6,8 @@ static const int gpsTxPin = 3;
 
 static void printGpsDateTime(TinyGPSDate &d, TinyGPSTime &t);
 
-static const double lastLat = 0;
-static const double lastLng = 0;
+double lastLat = 0;
+double lastLng = 0;
 
 // The TinyGPS++ object
 TinyGPSPlus gps;
@@ -61,6 +61,72 @@ static void getFloatValue(float val, bool valid, int len, int prec, char *result
   smartDelay(0);
 }
 
+int getGpshdopAndsateNum(int *satellitesNum, int *hdop)
+{
+  if (gps.satellites.isValid()) {
+    *satellitesNum = gps.satellites.value();
+    *hdop = gps.hdop.value();
+    return 0;
+  }
+
+  return -1;
+}
+int getGpslngAndlatValue(double *lat, double *lng, int *age)
+{
+  if (gps.location.isValid()) {
+    *lat =  gps.location.lat();
+    *lng = gps.location.lng();
+    lastLat = *lat;
+    lastLng = *lng;
+
+    *age = gps.location.age();
+    return 0;
+  }
+
+  return -1;
+}
+
+int getGpsAltitude(float *altitudeMeters)
+{
+  if (gps.altitude.isValid()) {
+    *altitudeMeters = gps.altitude.meters();
+    return 0;
+  }
+
+  return -1;
+}
+
+int getGpsTime(int *year, int *month, int *day, int *hour, int *minute, int *second)
+{
+  if (gps.date.isValid()) {
+    *year = gps.date.year();
+    *month = gps.date.month();
+    *day = gps.date.day();
+  } else {
+    return -1;
+  }
+
+  if (gps.time.isValid()) {
+    *hour = gps.time.hour();
+    *minute = gps.time.minute();
+    *second = gps.time.second();
+  } else {
+    return -1;
+  }
+
+  return 0;
+}
+
+void getGpsDistance(unsigned long *distance)
+{
+  *distance = (unsigned long)TinyGPSPlus::distanceBetween(
+                gps.location.lat(),
+                gps.location.lng(),
+                lastLat,
+                lastLng) / 1000;
+  printInt(*distance, gps.location.isValid(), 9);
+
+}
 
 void getGpsValue()
 {
